6b0ca09d45eb819a148fbc9e680cc01c8f68545f
fahad <fahad@fahad-300E4C-300E5C-300E7C.(none)>
2013-05-24 21:08:13 +0530

    working.
    Signed-off-by: fahad <fahad@fahad-300E4C-300E5C-300E7C.(none)>

--- src/com/jawsware/core/share/OverlayService.java
+++ src/com/jawsware/core/share/OverlayService.java
@@ -16,6 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import android.util.Log;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
